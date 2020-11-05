a396de43d158c66f279d482ff6dd98c738ad2b7d
Fernando Constantino <const.fernando@gmail.com>
2020-11-05 01:40:04 -0300

    fixed e-mail in prod

--- run.sh
+++ run.sh
@@ -125,16 +125,32 @@ if [ ! -d userver-web ] || [ "$USERVER_FORCE_BUILD" == "true" ]; then
   stop_and_remove_container userver-web
   clone_repo userver-web
 
-  envs=(
-    "s/SERVER_NAME=/SERVER_NAME=${USERVER_VIRTUAL_HOST} adminer.${USERVER_VIRTUAL_HOST} auth.${USERVER_VIRTUAL_HOST} filemgr.${USERVER_VIRTUAL_HOST} mail.${USERVER_VIRTUAL_HOST} webmail.${USERVER_VIRTUAL_HOST} whoami.${USERVER_VIRTUAL_HOST}/g"
-    #"s/AUTO_LETS_ENCRYPT=no/AUTO_LETS_ENCRYPT=no/g"
-    #"s/GENERATE_SELF_SIGNED_SSL=no/GENERATE_SELF_SIGNED_SSL=no/g"
-    #"s/HTTP2=yes/HTTP2=yes/g"
-    #"s/REDIRECT_HTTP_TO_HTTPS=no/REDIRECT_HTTP_TO_HTTPS=no/g"
-    #"s/DISABLE_DEFAULT_SERVER=yes/DISABLE_DEFAULT_SERVER=yes/g"
-    #"s/ALLOWED_METHODS=GET|POST|PATCH|DELETE|HEAD/ALLOWED_METHODS=GET|POST|PATCH|DELETE|HEAD/g"
-    #"s/SERVE_FILES=no/SERVE_FILES=no/g"
-  )
+  hosts="${USERVER_VIRTUAL_HOST} adminer.${USERVER_VIRTUAL_HOST} auth.${USERVER_VIRTUAL_HOST} filemgr.${USERVER_VIRTUAL_HOST} mail.${USERVER_VIRTUAL_HOST} webmail.${USERVER_VIRTUAL_HOST} whoami.${USERVER_VIRTUAL_HOST}/g"
+
+  if [ "$USERVER_MODE" == "prod" ]; then
+    envs=(
+      "s/SERVER_NAME=/SERVER_NAME=${hosts}/g"
+      "s/AUTO_LETS_ENCRYPT=no/AUTO_LETS_ENCRYPT=yes/g"
+      #"s/GENERATE_SELF_SIGNED_SSL=no/GENERATE_SELF_SIGNED_SSL=no/g"
+      #"s/HTTP2=yes/HTTP2=yes/g"
+      #"s/REDIRECT_HTTP_TO_HTTPS=no/REDIRECT_HTTP_TO_HTTPS=no/g"
+      #"s/DISABLE_DEFAULT_SERVER=yes/DISABLE_DEFAULT_SERVER=yes/g"
+      #"s/ALLOWED_METHODS=GET|POST|PATCH|DELETE|HEAD/ALLOWED_METHODS=GET|POST|PATCH|DELETE|HEAD/g"
+      #"s/SERVE_FILES=no/SERVE_FILES=no/g"
+    )
+  else
+    envs=(
+      "s/SERVER_NAME=/SERVER_NAME=${hosts}/g"
+      #"s/AUTO_LETS_ENCRYPT=no/AUTO_LETS_ENCRYPT=no/g"
+      "s/GENERATE_SELF_SIGNED_SSL=no/GENERATE_SELF_SIGNED_SSL=yes/g"
+      #"s/HTTP2=yes/HTTP2=yes/g"
+      #"s/REDIRECT_HTTP_TO_HTTPS=no/REDIRECT_HTTP_TO_HTTPS=no/g"
+      #"s/DISABLE_DEFAULT_SERVER=yes/DISABLE_DEFAULT_SERVER=yes/g"
+      #"s/ALLOWED_METHODS=GET|POST|PATCH|DELETE|HEAD/ALLOWED_METHODS=GET|POST|PATCH|DELETE|HEAD/g"
+      #"s/SERVE_FILES=no/SERVE_FILES=no/g"
+    )
+  fi
+
   cp userver-web/nginx-firewall/.env.template userver-web/nginx-firewall/.env
   sed_replace_occurences userver-web/nginx-firewall/.env "${envs[@]}"
 
